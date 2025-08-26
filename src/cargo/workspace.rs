b244a8eef22d0cc627bfa54ca2f6992261b35a76
Jessica Black <jess@attunehq.com>
2025-08-26 14:09:45 -0700

    fix executable bit on unix (#10)

--- src/cargo/workspace.rs
+++ src/cargo/workspace.rs
@@ -425,7 +425,13 @@ impl<'ws> ProfileDir<'ws, Locked> {
         standard
             .into_iter()
             .chain(dependencies)
-            .map(|(path, entry)| Artifact::builder().hash(&entry.hash).target(path).build())
+            .map(|(path, entry)| {
+                Artifact::builder()
+                    .target(path)
+                    .hash(&entry.hash)
+                    .executable(entry.executable)
+                    .build()
+            })
             .inspect(|artifact| trace!(?artifact, "enumerated artifact"))
             .collect::<Vec<_>>()
             .pipe(Ok)
