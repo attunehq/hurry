6a75d8c045107e998319d0694e4761b021a26e76
Jessica Black <jess@attunehq.com>
2025-10-07 14:00:51 -0700

    Fix comment in build scripts

--- packages/hurry/build.rs
+++ packages/hurry/build.rs
@@ -5,12 +5,12 @@ fn main() {
     sqlx_env_var_tests();
 }
 
-/// sqlx's compile-time macros and #[sqlx::test] macro use DATABASE_URL.
-/// This workaround allows us to use HURRY_DATABASE_URL in .env instead,
-/// avoiding conflicts with other packages (like courier) that also need databases.
+/// sqlx's compile-time macros and #[sqlx::test] macro use DATABASE_URL. This
+/// workaround allows us to use HURRY_DATABASE_URL in .env instead, avoiding
+/// conflicts with other packages (like courier) that also need databases.
 ///
-/// We only run this in debug builds so that it affects tests and local dev
-/// but not production; in release builds, DATABASE_URL needs to be set by the user.
+/// We only run this in debug builds so that it affects tests and local dev but
+/// not production; in release builds we don't use `DATABASE_URL`.
 #[cfg(debug_assertions)]
 fn sqlx_env_var_tests() {
     println!("cargo:rerun-if-env-changed=HURRY_DATABASE_URL");
