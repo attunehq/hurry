d3a6339bc24a6981e4ea6b29ac1b5523f17fd232
Jessica Black <jess@attunehq.com>
2025-10-09 17:31:02 -0700

    Support `hurry` and `courier` databases side-by-side (#59)

--- packages/hurry/build.rs
+++ packages/hurry/build.rs
@@ -6,15 +6,15 @@ fn main() {
 }
 
 /// sqlx's compile-time macros and #[sqlx::test] macro use DATABASE_URL. This
-/// workaround allows us to use COURIER_DATABASE_URL in .env instead, avoiding
-/// conflicts with other packages (like hurry) that also need databases.
+/// workaround allows us to use HURRY_DATABASE_URL in .env instead, avoiding
+/// conflicts with other packages (like courier) that also need databases.
 ///
 /// We only run this in debug builds so that it affects tests and local dev but
 /// not production; in release builds we don't use `DATABASE_URL`.
 #[cfg(debug_assertions)]
 fn sqlx_env_var_tests() {
-    println!("cargo:rerun-if-env-changed=COURIER_DATABASE_URL");
-    if let Ok(url) = std::env::var("COURIER_DATABASE_URL") {
+    println!("cargo:rerun-if-env-changed=HURRY_DATABASE_URL");
+    if let Ok(url) = std::env::var("HURRY_DATABASE_URL") {
         println!("cargo:rustc-env=DATABASE_URL={url}");
     }
 }
