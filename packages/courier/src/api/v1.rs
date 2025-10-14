e1f5e145d8b6757d494416764e2562976a3c7559
Jessica Black <jess@attunehq.com>
2025-10-14 10:04:20 -0700

    Courier: Distributed cargo cache (#67)

--- packages/courier/src/api/v1.rs
+++ packages/courier/src/api/v1.rs
@@ -2,11 +2,13 @@ use axum::{Router, routing::get};
 
 use crate::api::State;
 
+pub mod cache;
 pub mod cas;
 pub mod health;
 
 pub fn router() -> Router<State> {
     Router::new()
+        .nest("/cache", cache::router())
         .nest("/cas", cas::router())
         .route("/health", get(health::handle))
 }
