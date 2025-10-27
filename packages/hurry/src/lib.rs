0764698ef441912e0a5bcc93ae4d39c0709dbc05
Eliza Zhang <eliza@elizazhang.com>
2025-10-27 14:39:39 -0700

    refactor: Remove unused types

--- packages/hurry/src/lib.rs
+++ packages/hurry/src/lib.rs
@@ -4,21 +4,9 @@
 //! that configuration. It's only a library to enable sharing code in `hurry`
 //! with benchmarks and integration tests in the `hurry` repository.
 
-use derive_more::Display;
-
 pub mod cargo;
 pub mod cas;
 pub mod ext;
 pub mod fs;
 pub mod path;
 pub mod progress;
-
-/// The associated type's state is unlocked.
-/// Used for the typestate pattern.
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Display, Default)]
-pub struct Unlocked;
-
-/// The associated type's state is locked.
-/// Used for the typestate pattern.
-#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Display, Default)]
-pub struct Locked;
