4a90f2180dd236c43d8caabdd2f1e7629c9a876b
Jessica Black <jess@attunehq.com>
2025-10-28 14:39:53 -0700

    Add `hurry cache show` command to show the local cache directory location

--- packages/hurry/src/bin/hurry/cmd/cache.rs
+++ packages/hurry/src/bin/hurry/cmd/cache.rs
@@ -1,9 +1,13 @@
 use clap::Subcommand;
 
 pub mod reset;
+pub mod show;
 
 #[derive(Clone, Debug, Subcommand)]
 pub enum Command {
     /// Reset the cache.
     Reset(reset::Options),
+
+    /// Print the cache directory for the user.
+    Show,
 }
